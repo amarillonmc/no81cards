7de2759584661a0f113d44d8b54e35e05724fb68
Nemo Ma <nemoma@nmonolith.com>
2020-10-19 10:32:55 -0400

    fix

--- expansions/script/c9910057.lua
+++ expansions/script/c9910057.lua
@@ -88,7 +88,6 @@ function c9910057.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,g:GetCount(),0,0)
 end
 function c9910057.operation(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) then
 		Duel.SendtoHand(tc,nil,REASON_EFFECT)
