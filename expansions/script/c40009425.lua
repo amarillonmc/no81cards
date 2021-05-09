afa47f5d2aa8f9fddf43e0d39d6462432dade91c
Nemo Ma <nemoma@nmonolith.com>
2021-05-09 10:29:24 -0400

    fix

--- expansions/script/c40009425.lua
+++ expansions/script/c40009425.lua
@@ -1,16 +1,36 @@
 --机械加工·起
-if not pcall(function() require("expansions/script/c40008000") end) then require("script/c40008000") end
-local m,cm=rscf.DefineCard(40009425)
-function cm.initial_effect(c)
-	local e1=rsef.ACT(c,nil,nil,{1,m,1},"tg,sp",nil,nil,nil,rsop.target2(rstg.opinfo("sp",0,0,LOCATION_DECK),cm.tgfilter,"tg",LOCATION_DECK),cm.act)
+function c40009425.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(40009425,0))
+	e1:SetCategory(CATEGORY_TOGRAVE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,40009425+EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(c40009425.target)
+	e1:SetOperation(c40009425.activate)
+	c:RegisterEffect(e1)
 end
-function cm.tgfilter(c)
-	return c:IsAbleToGrave() and c:IsRace(RACE_INSECT)
+function c40009425.tgfilter(c)
+	return c:IsRace(RACE_INSECT) and c:IsAbleToGrave()
 end
-function cm.act(e,tp)
-	local ct,og,tc=rsop.SelectToGrave(tp,cm.tgfilter,tp,LOCATION_DECK,0,1,1,nil,{})
-	if tc and tc:IsLocation(LOCATION_GRAVE) and tc:IsType(TYPE_NORMAL) then
-		rsop.SelectOC("sp",true)
-		rsop.SelectSpecialSummon(tp,rscf.spfilter2(Card.IsSetCard,0x5f1d),tp,LOCATION_DECK,0,1,1,nil,{},e,tp)
+function c40009425.spfilter(c,e,tp)
+	return c:IsSetCard(0x5f1d) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c40009425.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c40009425.tgfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+end
+function c40009425.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,c40009425.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+	local tc=g:GetFirst()
+	if tc and Duel.SendtoGrave(tc,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_GRAVE) and tc:IsRace(RACE_INSECT) and tc:IsType(TYPE_NORMAL)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(c40009425.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
+		and Duel.SelectYesNo(tp,aux.Stringid(40009425,1)) then
+		Duel.BreakEffect()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g1=Duel.SelectMatchingCard(tp,c40009425.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+		Duel.SpecialSummon(g1,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
