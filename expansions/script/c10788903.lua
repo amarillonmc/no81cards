8627f652270ff117db049704a9add3c97e7be61d
Nemo Ma <nemoma@nmonolith.com>
2022-06-06 07:04:11 -0400

    Create c10788903.lua

---
+++ expansions/script/c10788903.lua
@@ -0,0 +1,89 @@
+--混沌的魔救勘察者
+local m=10788903
+local cm=_G["c"..m]
+
+function cm.initial_effect(c)
+	--xyz summon
+	c:EnableReviveLimit()
+	aux.AddXyzProcedure(c,nil,4,2,nil,nil,7)
+	--spsummon2
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(m,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,m)
+	e1:SetTarget(cm.sptg2)
+	e1:SetOperation(cm.spop2)
+	c:RegisterEffect(e1)
+	--special summon or tohand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(m,1))
+	e2:SetCategory(CATEGORY_SEARCH+CATEGORY_SPECIAL_SUMMON+CATEGORY_DECKDES)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,m+1)
+	e2:SetCost(cm.spcost)
+	e2:SetTarget(cm.sptg)
+	e2:SetOperation(cm.spop)
+	c:RegisterEffect(e2)
+end
+
+function cm.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>4 end
+end
+
+function cm.spfilter(c,e,tp)
+	return not c:IsType(TYPE_TUNER) and c:IsLevelBelow(4) and (c:IsRace(RACE_ROCK) or c:IsSetCard(0x773)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+
+function cm.spop2(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)<=4 then return end
+	Duel.ConfirmDecktop(tp,5)
+	local g=Duel.GetDecktopGroup(tp,5)
+	local ct=g:GetCount()
+	if ct>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and g:FilterCount(cm.spfilter,nil,e,tp)>0
+		and Duel.SelectYesNo(tp,aux.Stringid(m,2)) then
+		Duel.DisableShuffleCheck()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sg=g:FilterSelect(tp,cm.spfilter,1,1,nil,e,tp)
+		Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+		ct=g:GetCount()-sg:GetCount()
+	end
+	if ct>0 then
+		Duel.SortDecktop(tp,tp,ct)
+		for i=1,ct do
+			local mg=Duel.GetDecktopGroup(tp,1)
+			Duel.MoveSequence(mg:GetFirst(),SEQ_DECKBOTTOM)
+		end
+	end
+end
+
+function cm.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,2,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,2,2,REASON_COST)
+end
+
+function cm.spfilter2(c,e,tp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	return c:IsSetCard(0x773,0x140) and ((ft>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false)) or c:IsAbleToHand())
+end
+
+function cm.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(cm.spfilter2,tp,LOCATION_DECK,0,1,nil,e,tp) end
+end
+
+function cm.spop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_OPERATECARD)
+	local g=Duel.SelectMatchingCard(tp,cm.spfilter2,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local tc=g:GetFirst()
+	if tc then
+		if tc:IsAbleToHand() and (not tc:IsCanBeSpecialSummoned(e,0,tp,false,false) or ft<=0 or Duel.SelectOption(tp,1190,1152)==0) then
+			Duel.SendtoHand(tc,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,tc)
+		else
+			Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end
