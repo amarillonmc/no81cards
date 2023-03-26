3279c7f752a3327e239be55e9db6791bf6a05ad5
Nemo Ma <nemoma@nmonolith.com>
2023-03-26 17:32:58 -0400

    upd

--- expansions/script/c33200002.lua
+++ expansions/script/c33200002.lua
@@ -2,7 +2,6 @@
 function c33200002.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,33200002+EFFECT_COUNT_CODE_OATH)
