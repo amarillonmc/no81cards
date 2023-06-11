ac7f26b174c23943154ffc0c274d6fcff5cc719e
Nemo Ma <nemoma@nmonolith.com>
2023-06-11 11:02:51 -0400

    upd

--- expansions/script/c10122003.lua
+++
@@ -1,6 +0,0 @@
---乌托兰秘境 星辉原野
-local s,id = GetID()
-if not pcall(function() require("expansions/script/c10122001") end) then require("script/c10122001") end
-function s.initial_effect(c)
-	Scl_Utoland.FieldEffects(c, id, { "PlayerCost", "Discard", 1 }, nil, 1, 2)
-end
