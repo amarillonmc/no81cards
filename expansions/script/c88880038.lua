c2c754b915bf955d7c157637d0cbcfe24caf5d3e
polymer1212 <937868195@qq.com>
2023-09-11 15:55:04 +0800

    delete cards

--- expansions/script/c88880038.lua
+++
@@ -1,6 +0,0 @@
---神蚀创痕-瓦尔基里
-function c88880038.initial_effect(c)
-	--fusion material
-	c:EnableReviveLimit()
-	aux.AddFusionProcFunRep(c,aux.FilterBoolFunction(Card.IsFusionSetCard,0xc06),2,true)
-end
--- expansions/script/c88880042.lua
+++
@@ -1,8 +0,0 @@
---神蚀创痕-瓦尔基里·乌拉诺斯
-function c88880042.initial_effect(c)
-	c:EnableReviveLimit()
-	aux.AddFusionProcFun2(c,c88880042.matfilter,aux.FilterBoolFunction(Card.IsFusionSetCard,0xc06),true)  
-end
-function c88880042.matfilter(c)
-	return c:IsFusionSetCard(0xc06) and c:IsLevelAbove(8)
-end
