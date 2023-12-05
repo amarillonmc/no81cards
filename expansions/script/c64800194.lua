52f7c5de0feee13e03264352810f8bdc9f988121
polymer1212 <937868195@qq.com>
2023-12-05 11:23:04 +0800

    fix

--- expansions/script/c64800194.lua
+++ expansions/script/c64800194.lua
@@ -3,7 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(m,0))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
@@ -16,15 +16,12 @@ function s.filter(c,e,tp)
 	return c:IsCanBeSpecialSummoned(e,0,tp,false,false) and Duel.IsExistingMatchingCard(s.eqfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,c)
 end
 function s.eqfilter(c,tc)
-	return not c:IsRace(tc:GetRace()) and  not c:IsAttribute(tc:GetAttribute())  and c:IsSetCard(0x412)
+	return not c:IsRace(tc:GetRace()) and not c:IsAttribute(tc:GetAttribute()) and c:IsSetCard(0x412) and c:IsType(TYPE_MONSTER)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp)  end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
-end
-function s.filter2(c,e,tp)
-	return c:IsCanBeSpecialSummoned(e,0,tp,false,false) 
 end
 function s.actcfilter(c)
 	return c:IsSetCard(0x412) and c:IsFaceup() and c:IsType(TYPE_XYZ)
@@ -33,13 +30,13 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
 	local tc=g:GetFirst()
 	if tc then
 		Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP)
-		if Duel.IsExistingMatchingCard(s.eqfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,tc) then 
+		if Duel.IsExistingMatchingCard(aux.NecroValleyFilter(s.eqfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil,tc) then 
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-			local eqg=Duel.SelectMatchingCard(tp,s.eqfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,tc)
+			local eqg=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.eqfilter),tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,tc)
 			local eqc=eqg:GetFirst()
 			if not Duel.Equip(tp,eqc,tc) then return end
 			--equip limit
