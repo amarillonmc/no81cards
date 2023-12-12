2fe1fd2505efa51dd070ae9099e14215c822f323
polymer1212 <937868195@qq.com>
2023-12-12 11:51:05 +0800

    fix

--- expansions/script/c60001268.lua
+++ expansions/script/c60001268.lua
@@ -34,6 +34,7 @@ function cm.activate1(e,tp,eg,ep,ev,re,r,rp)
 	local tp=eg:GetFirst():GetOwner()
 	Duel.NegateSummon(eg)
 	if Duel.SendtoDeck(eg,nil,2,REASON_EFFECT)~=0 then
+		Debug.Message("1")
 		local bg=Duel.GetOperatedGroup()
 		local tbg=bg:Filter(Card.IsLocation,nil,LOCATION_DECK+LOCATION_EXTRA)
 		local t=#tbg
@@ -43,15 +44,17 @@ function cm.activate1(e,tp,eg,ep,ev,re,r,rp)
 			ncode[i]=nc:GetCode()
 			nc=tbg:GetNext()
 		end
-		local csg=Duel.GetMatchingGroup(cm.fil,tp,LOCATION_DECK,0,nil,t)
+		Debug.Message(t)
+		local csg=Duel.GetMatchingGroup(cm.fil,tp,LOCATION_DECK,0,nil,t,e,tp)
 		local spg=csg:RandomSelect(tp,math.min(#csg,t))
+		Duel.SpecialSummon(spg,0,tp,tp,false,false,POS_FACEUP)
 		if #csg<t then
 			Duel.Draw(tp,t-#csg,REASON_EFFECT)
 		end
 	end
 end
 
-function cm.fil(c,t)
+function cm.fil(c,t,e,tp)
 	local ccode=0
 	for i=1,t do
 		if ncode[i]==c:GetCode() then
