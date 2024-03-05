55e3fbbc568f7afcfa0435a285f4c030866779db
polymer1212 <937868195@qq.com>
2024-03-06 01:59:37 +0800

    fix

--- expansions/script/c12899017.lua
+++ expansions/script/c12899017.lua
@@ -1,12 +1,16 @@
 --D.H.P.K.-罪天使安吉拉
-function c12899017.initial_effect(c)
-	--limtcon
-	local e1=Effect.CreateEffect(c)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_CANNOT_TRIGGER)
-	e1:SetCondition(c12899017.limitcon)
-	c:RegisterEffect(e1)
+local s,id,o=GetID()
+function s.initial_effect(c)
+	if not s.global_flag then
+		s.global_flag=true
+		--act limit
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_CANNOT_TRIGGER)
+		e1:SetTargetRange(0xff,0xff)
+		e1:SetTarget(s.disable)
+		Duel.RegisterEffect(e1,0)
+	end
 	--spsummon
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(12899017,0))
@@ -16,9 +20,9 @@ function c12899017.initial_effect(c)
 	e2:SetRange(LOCATION_HAND)
 	e2:SetCountLimit(1,12899017)
 	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
-	e2:SetCondition(c12899017.sumcon)
-	e2:SetTarget(c12899017.sumtg)
-	e2:SetOperation(c12899017.sumop)
+	e2:SetCondition(s.sumcon)
+	e2:SetTarget(s.sumtg)
+	e2:SetOperation(s.sumop)
 	c:RegisterEffect(e2)
 	--
 	local e3=Effect.CreateEffect(c)
@@ -27,79 +31,86 @@ function c12899017.initial_effect(c)
 	e3:SetCode(EVENT_FREE_CHAIN)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetCountLimit(1,12899017)
-	e3:SetCondition(c12899017.condition)
-	e3:SetTarget(c12899017.target)
-	e3:SetOperation(c12899017.operation)
+	e3:SetCondition(s.condition)
+	e3:SetTarget(s.target)
+	e3:SetOperation(s.operation)
 	c:RegisterEffect(e3)
 end
-function c12899017.ndcfilter(c)
+s.limt_name_hpk=id
+function s.disable(e,c)
+	if c.limt_name_hpk~=id then return end
+	local tp=c:GetControler()
+	return (c:IsType(TYPE_EFFECT) or c:GetOriginalType()&TYPE_EFFECT~=0)
+		and Duel.IsExistingMatchingCard(s.ndcfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+function s.ndcfilter(c)
 	return c:IsFacedown() or not c:IsSetCard(0x5a71)
 end
-function c12899017.limitcon(e)
+function s.limitcon(e)
 	return e:GetHandler():IsLocation(LOCATION_MZONE)
-		and Duel.IsExistingMatchingCard(c12899017.ndcfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+		and Duel.IsExistingMatchingCard(s.ndcfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
 end
-function c12899017.cfilter(c)
+function s.cfilter(c)
 	return c:IsFaceup() and c:IsSetCard(0x5a71)
 end
-function c12899017.sumcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(c12899017.cfilter,tp,LOCATION_MZONE,0,1,nil)
+function s.sumcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
 end
-function c12899017.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1 
 		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
-function c12899017.sumop(e,tp,eg,ep,ev,re,r,rp)
+function s.sumop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 then
 		Duel.BreakEffect()
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
-		local g=Duel.SelectMatchingCard(tp,c12899017.setfilter,tp,LOCATION_DECK,0,1,1,nil)
+		local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil)
 		if g:GetCount()>0 then
 			Duel.SSet(tp,g:GetFirst())
 		end
 	end
 end
-function c12899017.setfilter(c)
+function s.setfilter(c)
 	return c:IsSetCard(0x6a71) and c:IsType(TYPE_TRAP) and c:IsSSetable()
 end
-function c12899017.condition(e,tp,eg,ep,ev,re,r,rp)
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentPhase()==PHASE_MAIN1 or Duel.GetCurrentPhase()==PHASE_MAIN2
 end
-function c12899017.filter1(c,e)
+function s.filter1(c,e)
 	return c:IsOnField() and not c:IsImmuneToEffect(e)
 end
-function c12899017.filter2(c,e,tp,m,f,gc,chkf)
+function s.filter2(c,e,tp,m,f,gc,chkf)
 	return c:IsSetCard(0xda71) and c:IsType(TYPE_FUSION) and (not f or f(c))
 		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,gc,chkf)
 end
-function c12899017.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then
 		local chkf=tp
 		local mg1=Duel.GetFusionMaterial(tp):Filter(Card.IsOnField,nil)
-		local res=Duel.IsExistingMatchingCard(c12899017.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil,c,chkf)
+		local res=Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg1,nil,c,chkf)
 		if not res then
 			local ce=Duel.GetChainMaterial(tp)
 			if ce~=nil then
 				local fgroup=ce:GetTarget()
 				local mg2=fgroup(ce,e,tp)
 				local mf=ce:GetValue()
-				res=Duel.IsExistingMatchingCard(c12899017.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg2,mf,c,chkf)
+				res=Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,mg2,mf,c,chkf)
 			end
 		end
 		return res
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
-function c12899017.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local chkf=tp
 	if not c:IsRelateToEffect(e) or c:IsImmuneToEffect(e) then return end
-	local mg1=Duel.GetFusionMaterial(tp):Filter(c12899017.filter1,nil,e)
-	local sg1=Duel.GetMatchingGroup(c12899017.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,c,chkf)
+	local mg1=Duel.GetFusionMaterial(tp):Filter(s.filter1,nil,e)
+	local sg1=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg1,nil,c,chkf)
 	local mg2=nil
 	local sg2=nil
 	local ce=Duel.GetChainMaterial(tp)
@@ -107,7 +118,7 @@ function c12899017.operation(e,tp,eg,ep,ev,re,r,rp)
 		local fgroup=ce:GetTarget()
 		mg2=fgroup(ce,e,tp)
 		local mf=ce:GetValue()
-		sg2=Duel.GetMatchingGroup(c12899017.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg2,mf,c,chkf)
+		sg2=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg2,mf,c,chkf)
 	end
 	if sg1:GetCount()>0 or (sg2~=nil and sg2:GetCount()>0) then
 		local sg=sg1:Clone()
