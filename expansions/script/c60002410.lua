54cedb91bf40410b8918f1f1bc16997164fd7dca
polymer1212 <937868195@qq.com>
2024-05-24 23:26:50 +0800

    fix

--- expansions/script/c60002410.lua
+++ expansions/script/c60002410.lua
@@ -1,39 +1,24 @@
 --瓶中之人
 local cm,m,o=GetID()
 function cm.initial_effect(c)
-	--tograve
-	local e3=Effect.CreateEffect(c)
-	e3:SetCategory(CATEGORY_TOGRAVE)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e3:SetCode(EVENT_REMOVE)
-	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-	e3:SetCountLimit(1,m)
-	e3:SetOperation(cm.tgop)
-	c:RegisterEffect(e3)
-	
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e3:SetCode(EVENT_REMOVE)
-	e3:SetOperation(cm.tgop2)
-	c:RegisterEffect(e3)
-
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetCategory(CATEGORY_TOHAND)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	--e1:SetCondition(cm.condition)
+	e1:SetCountLimit(1,m)
 	e1:SetOperation(cm.activate)
 	c:RegisterEffect(e1)
-end
-function cm.tgop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.SendtoDeck(e:GetHandler(),nil,REASON_EFFECT)
-end
-function cm.tgop2(e,tp,eg,ep,ev,re,r,rp)
-	Duel.RegisterFlagEffect(tp,m,RESET_PHASE+PHASE_END,0,1)
-end
-function cm.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetFlagEffect(tp,m)~=0
+	--tograve
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_TODECK)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_REMOVE)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e2:SetCountLimit(1,m+10000000)
+	e2:SetTarget(cm.tdtg)
+	e2:SetOperation(cm.tdop)
+	c:RegisterEffect(e2)
 end
 function cm.activate(e,tp,eg,ep,ev,re,r,rp)
 	--spsm
@@ -57,5 +42,15 @@ function cm.spop(e,tp,eg,ep,ev,re,r,rp)
 		else
 			Duel.RegisterFlagEffect(tp,m+10000000,RESET_PHASE+PHASE_END,0,1)
 		end
+	end
+end
+function cm.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToDeck() end
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,e:GetHandler(),1,0,0)
+end
+function cm.tdop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SendtoDeck(c,nil,SEQ_DECKTOP,REASON_EFFECT)
 	end
 end
