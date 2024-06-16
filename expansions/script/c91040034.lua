47d8119da57ca345aadec470812f7ddbd7a3fc8c
polymer1212 <937868195@qq.com>
2024-06-16 18:09:17 +0800

    fix

--- expansions/script/c91040034.lua
+++ expansions/script/c91040034.lua
@@ -20,7 +20,7 @@ function c91040034.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1,m*2)
+	e2:SetCountLimit(1,m+100)
 	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
 	e2:SetTarget(cm.destg)
 	e2:SetOperation(cm.desop)
@@ -30,9 +30,9 @@ function cm.spcostfilter(c,tp)
 	return c:IsFaceup()  and c:IsRace(RACE_ZOMBIE)and c:IsAbleToGraveAsCost() and Duel.GetMZoneCount(tp,c)>0
 end
 function cm.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return  Duel.IsExistingMatchingCard(cm.spcostfilter,tp,LOCATION_MZONE,0,1,nil,c,tp,Card.IsAbleToGraveAsCost) end
+	if chk==0 then return  Duel.IsExistingMatchingCard(cm.spcostfilter,tp,LOCATION_MZONE,0,1,e:GetHandler(),c,tp,Card.IsAbleToGraveAsCost) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,cm.spcostfilter,tp,LOCATION_MZONE,0,1,1,nil,tp)
+	local g=Duel.SelectMatchingCard(tp,cm.spcostfilter,tp,LOCATION_MZONE,0,1,1,e:GetHandler(),tp)
 	Duel.SendtoGrave(g,REASON_COST)
 end
 function cm.filter(c,e,tp)
@@ -50,16 +50,14 @@ function cm.operation(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
-function cm.desfilter1(c)
-	return Duel.IsExistingMatchingCard(nil,0,LOCATION_ONFIELD,LOCATION_ONFIELD,1,c)
-end
 function cm.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(cm.desfilter1,tp,LOCATION_ONFIELD,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
 	local g=Duel.GetMatchingGroup(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
 function cm.desop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
 	local g2=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,g1)
+	Duel.HintSelection(g2)
 	Duel.Destroy(g2,REASON_EFFECT)
 end
