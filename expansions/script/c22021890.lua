edd7e5391e846a6b8b4d9ba64367b15285a28d1f
polymer1212 <937868195@qq.com>
2024-06-26 21:54:39 +0800

    fix

--- expansions/script/c22021890.lua
+++ expansions/script/c22021890.lua
@@ -1,9 +1,8 @@
 --人理之诗 银之键
 function c22021890.initial_effect(c)
-
-
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(22021880,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
@@ -12,13 +11,24 @@ function c22021890.initial_effect(c)
 	e1:SetTarget(c22021890.target)
 	e1:SetOperation(c22021890.activate)
 	c:RegisterEffect(e1)
+	--material
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(22021880,1))
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCountLimit(1,22021891)
+	e3:SetTarget(c22021890.mattg)
+	e3:SetOperation(c22021890.matop)
+	c:RegisterEffect(e3)
 end
-function c22021890.cfilter(c,tp)
+function c22021890.rfilter(c,tp)
 	return Duel.GetMZoneCount(tp,c)>0
 end
 function c22021890.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckReleaseGroup(REASON_COST,tp,c22021890.cfilter,1,nil,tp) end
-	local g=Duel.SelectReleaseGroup(REASON_COST,tp,c22021890.cfilter,1,1,nil,tp)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,c22021890.rfilter,1,nil,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	local g=Duel.SelectReleaseGroup(tp,c22021890.rfilter,1,1,nil,tp)
 	Duel.Release(g,REASON_COST)
 end
 function c22021890.target(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -32,4 +42,22 @@ function c22021890.activate(e,tp,eg,ep,ev,re,r,rp)
 		or not Duel.IsPlayerCanSpecialSummonMonster(tp,22021890,0,TYPE_NORMAL,0,0,9,RACE_FIEND,ATTRIBUTE_DARK) then return end
 	c:AddMonsterAttribute(TYPE_EFFECT+TYPE_SPELL)
 	Duel.SpecialSummon(c,SUMMON_VALUE_SELF,tp,tp,true,false,POS_FACEUP)
+end
+function c22021890.matfilter(c)
+	return c:IsFaceup() and c:IsRank(9) and c:IsType(TYPE_XYZ)
+end
+function c22021890.mattg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c22021890.matfilter(chkc) end
+	if chk==0 then return not e:GetHandler():IsStatus(STATUS_CHAINING)
+		and Duel.IsExistingTarget(c22021890.matfilter,tp,LOCATION_MZONE,0,1,nil)
+		and e:GetHandler():IsCanOverlay() end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,c22021890.matfilter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function c22021890.matop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then
+		Duel.Overlay(tc,Group.FromCards(c))
+	end
 end
