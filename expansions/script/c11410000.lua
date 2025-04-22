3e5b62df78bec8bb41eb296d66af4402ff01e1f0
polymer1212 <937868195@qq.com>
2025-04-23 02:51:58 +0800

    fix

--- expansions/script/c11410000.lua
+++ expansions/script/c11410000.lua
@@ -175,7 +175,7 @@ function cm.op(e,tp,eg,ep,ev,re,r,rp)
 					return _SetProperty(pe,prop1,prop2)
 				end
 				Effect.Clone=function(pe)
-					local ce=pe:Clone()
+					local ce=_Clone(pe)
 					if cm.proeffects[pe] then
 						cm.proeffects[ce]=cm.proeffects[pe]
 					end
