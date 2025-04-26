c3ae8a11b5fc5da156db39916e1e2f94f3c9ada8
polymer1212 <937868195@qq.com>
2025-04-26 12:42:59 +0800

    fix

--- expansions/script/c71000176.lua
+++ expansions/script/c71000176.lua
@@ -18,11 +18,11 @@ function c71000176.initial_effect(c)
 	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetTargetRange(LOCATION_MZONE,0)
-	e2:SetTarget(c71000176.target)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsCode,71000100))
+	e2:SetValue(1)
 	c:RegisterEffect(e2)
 	local e3=e2:Clone()
 	e3:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-	e3:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
 	c:RegisterEffect(e3)
 	local e4=e2:Clone()
 	e4:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
@@ -37,7 +37,6 @@ function c71000176.initial_effect(c)
 	e12:SetRange(LOCATION_GRAVE)
 	e12:SetCondition(c71000176.spcon)
 	c:RegisterEffect(e12)
-	--splimit
 	local e41=Effect.CreateEffect(c)
 	e41:SetType(EFFECT_TYPE_FIELD)
 	e41:SetRange(LOCATION_MZONE)
@@ -68,9 +67,7 @@ function c71000176.op(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 --2
-function c71000176.target(e,c)
-	return c:IsCode(71000100) and c:IsFaceup()
-end
+
 
 --3
 function c71000176.cfilter(c)
