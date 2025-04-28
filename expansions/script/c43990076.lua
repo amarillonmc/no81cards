34fd84832b53b28492a741c8e751b1056464afa4
polymer1212 <937868195@qq.com>
2025-04-28 22:24:33 +0800

    fix

--- expansions/script/c43990076.lua
+++ expansions/script/c43990076.lua
@@ -64,7 +64,7 @@ function c43990076.filter1(c,e)
 	return c:IsOnField() and c:IsAbleToRemove() and not c:IsImmuneToEffect(e)
 end
 function c43990076.filter2(c,e,tp,m,f,chkf)
-	return c:IsType(TYPE_FUSION) and c:IsRace(RACE_ILLUSION) and (not f or f(c))
+	return c:IsType(TYPE_FUSION) and c:IsSetCard(0x5510) and (not f or f(c))
 		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false) and c:CheckFusionMaterial(m,nil,chkf)
 end
 function c43990076.filter3(c)
