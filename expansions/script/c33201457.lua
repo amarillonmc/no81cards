1e08f2d2cece836daed0f4b09b2eb6ceafc15928
polymer1212 <937868195@qq.com>
2025-05-10 22:30:57 +0800

    fix

--- expansions/script/c33201457.lua
+++ expansions/script/c33201457.lua
@@ -7,7 +7,7 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--Position+Negate
 	local e1=Effect.CreateEffect(c)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER)
