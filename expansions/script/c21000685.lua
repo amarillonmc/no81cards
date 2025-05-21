77ee54fad29b0f1a30fff31b2f9c0e388e224d16
polymer1212 <937868195@qq.com>
2025-05-21 13:43:33 +0800

    fix

--- expansions/script/c21000685.lua
+++ expansions/script/c21000685.lua
@@ -97,7 +97,7 @@ function s.tfilter(c)
 end
 function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then 
-		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) and Duel.IsExistingMatchingCard(s.tfilter,tp,0,LOCATION_ONFIELD,1,nil)
+		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 end
