a68d4567fef4bbc3f10edf2fea1bdbf845c54be3
polymer1212 <937868195@qq.com>
2025-06-18 13:06:30 +0800

    fix

---
+++ expansions/script/c95101060 .lua
@@ -0,0 +1,74 @@
+--黑之裁判<教戒师>艾兹瓦尔德
+function c95101060.initial_effect(c)
+	--to deck
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(95101060,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TODECK+CATEGORY_GRAVE_ACTION)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,95101060)
+	e1:SetCondition(c95101060.spcon)
+	e1:SetTarget(c95101060.sptg)
+	e1:SetOperation(c95101060.spop)
+	c:RegisterEffect(e1)
+	--search
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(95101060,1))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCountLimit(1,95101060+1)
+	e2:SetCost(c95101060.thcost)
+	e2:SetTarget(c95101060.thtg)
+	e2:SetOperation(c95101060.thop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+end
+function c95101060.chkfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsPreviousLocation(LOCATION_DECK+LOCATION_EXTRA)
+end
+function c95101060.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c95101060.chkfilter,1,nil) and not eg:IsContains(e:GetHandler())
+		and Duel.IsExistingMatchingCard(Card.IsType,tp,0,LOCATION_GRAVE,5,eg,TYPE_MONSTER)
+end
+function c95101060.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetMZoneCount(tp)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function c95101060.tdfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsAbleToDeck() and aux.NecroValleyFilter()(c)
+end
+function c95101060.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)~=0 and Duel.IsExistingMatchingCard(c95101060.tdfilter,tp,0,LOCATION_GRAVE,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(95101060,2)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+		local tg=Duel.SelectMatchingCard(tp,c95101060.tdfilter,tp,0,LOCATION_GRAVE,1,3,nil)
+		Duel.HintSelection(tg)
+		Duel.SendtoDeck(tg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+	end
+end
+function c95101060.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(1-tp,3) end
+	Duel.DiscardDeck(1-tp,3,REASON_COST)
+end
+function c95101060.thfilter(c)
+	return c:IsSetCard(0xbbb) and not c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function c95101060.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c95101060.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c95101060.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local tc=Duel.SelectMatchingCard(tp,c95101060.thfilter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
+	if tc then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,tc)
+	end
+end
