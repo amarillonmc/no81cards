cfc20f8a18e0e045591346b4dab6ace080a54ed8
polymer1212 <937868195@qq.com>
2025-07-10 10:55:36 +0800

    fix

--- expansions/script/c88897040.lua
+++ expansions/script/c88897040.lua
@@ -63,12 +63,15 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.BreakEffect()
 	Duel.Destroy(e:GetHandler(),REASON_EFFECT)
 end
+function s.filter2(c)
+	return c:IsAttribute(ATTRIBUTE_FIRE) and c:IsRace(RACE_WARRIOR) and c:IsFaceup()
+end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_MZONE,0,e:GetHandler())
 	local c=e:GetHandler()
-	return c:IsReason(REASON_LOST_TARGET) and c:GetPreviousEquipTarget():IsLocation(LOCATION_GRAVE)
+	return c:IsReason(REASON_LOST_TARGET) and c:GetPreviousEquipTarget():IsLocation(LOCATION_GRAVE) and #g>0
 end
 function s.op(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD) then return end
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e1:SetCode(EVENT_CHAIN_SOLVING)
