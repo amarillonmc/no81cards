c336341978907c6cbf7b4dcceaaf08a0a7174cde
polymer1212 <937868195@qq.com>
2025-09-05 00:39:26 +0800

    fix

--- expansions/script/c40020186.lua
+++ expansions/script/c40020186.lua
@@ -23,7 +23,7 @@ function cm.initial_effect(c)
 	c:RegisterEffect(e2)
 end
 function cm.costfilter(c)
-	return c:IsCode(m,72880377) and c:IsAbleToGraveAsCost()
+	return c:IsCode(m,53129443,72880377) and c:IsAbleToGraveAsCost()
 end
 function cm.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(cm.costfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil) end
