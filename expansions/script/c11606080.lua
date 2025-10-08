3f354a230519127eb95d2478ce3c13db1f9ee5b9
polymer1212 <937868195@qq.com>
2025-10-08 22:26:57 +0800

    fix

--- expansions/script/c11606080.lua
+++ expansions/script/c11606080.lua
@@ -5,6 +5,7 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
 	c:RegisterEffect(e1)
 	--双方主要阶段才能发动。从手卡把1只「绝海滋养」怪兽召唤。
 	--「魔弹-血色之冠」「抗锯齿星人」
@@ -15,24 +16,34 @@ function s.initial_effect(c)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetCountLimit(1,id)
-	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER)
+	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
 	e2:SetCondition(s.condition)
 	e2:SetTarget(s.sumtg)
 	e2:SetOperation(s.sumop)
 	c:RegisterEffect(e2)
 	--自己作「绝海滋养」怪兽上级召唤的场合，可以把自己场上任意数量怪兽和对方场上1张卡解放来上级召唤。
 	--「帝王的烈旋」「吸血鬼吸食者」
+	local ce=Effect.CreateEffect(c)
+	ce:SetType(EFFECT_TYPE_FIELD)
+	ce:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+	ce:SetCode(id)
+	ce:SetRange(LOCATION_SZONE)
+	ce:SetTargetRange(0,LOCATION_MZONE)
+	c:RegisterEffect(ce)
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD)
 	e3:SetProperty(EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e3:SetCode(EFFECT_EXTRA_RELEASE_SUM)
 	e3:SetRange(LOCATION_HAND)
 	e3:SetTargetRange(0,LOCATION_MZONE)
-	e3:SetCountLimit(1,id+o)
+	e3:SetCondition(s.rcon)
+	e3:SetTarget(s.rtg)
+	e3:SetCountLimit(1)
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_GRANT)
 	e4:SetRange(LOCATION_SZONE)
 	e4:SetTargetRange(LOCATION_HAND,0)
+	e4:SetCondition(s.gecon)
 	e4:SetTarget(s.getg)
 	e4:SetLabelObject(e3)
 	c:RegisterEffect(e4)
@@ -57,6 +68,21 @@ function s.sumop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.Summon(tp,tc,true,nil)
 	end
 end
+function s.gecon(e)
+	return Duel.GetFlagEffect(tp,id)==0
+end
+
 function s.getg(e,c)
 	return c:IsSetCard(0x5225) and c:IsType(TYPE_MONSTER)
+end
+function s.rcon(e)
+	local tp=e:GetHandlerPlayer()
+	if Duel.GetFlagEffect(tp,id)~=0 then return false end
+	if not e:CheckCountLimit(e:GetHandlerPlayer()) then
+		Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+	end
+	return true
+end
+function s.rtg(e,c)
+	return c:IsHasEffect(id)
 end
