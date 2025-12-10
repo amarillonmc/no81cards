aff39b08a93c0569b588fac220a4627f3d741fdd
polymer1212 <937868195@qq.com>
2025-12-11 01:01:25 +0800

    fix

--- expansions/script/c89390110.lua
+++ expansions/script/c89390110.lua
@@ -2,6 +2,7 @@
 local s,id,o=GetID()
 function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_REMOVE)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(LOCATION_HAND+LOCATION_MZONE)
