3ac0bc4c8d93f569d9d3a9636464474ef13df520
polymer1212 <937868195@qq.com>
2026-03-01 12:22:24 +0800

    fix

--- expansions/script/c65820000.lua
+++ expansions/script/c65820000.lua
@@ -2,6 +2,7 @@
 local s,id,o=GetID()
 function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
