e8d05bbdbb307ed09ff73093be84c5c2ff931a4b
polymer1212 <937868195@qq.com>
2026-05-27 11:24:39 +0800

    fix

--- expansions/script/c40020794.lua
+++ expansions/script/c40020794.lua
@@ -66,19 +66,23 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function s.gravecon(e,tp,eg,ep,ev,re,r,rp)
-	if not re then return false end
-	return e:GetHandler():IsReason(REASON_EFFECT) and s.DarkSnake(re:GetHandler())
+	local c=e:GetHandler()
+	local rc=c:GetReasonEffect()
+	return c:IsReason(REASON_EFFECT) and rc and s.DarkSnake(rc:GetHandler())
 end
+
 function s.gravetg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsSSetable() end
 	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,e:GetHandler(),1,0,0)
 end
+
 function s.graveop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and Duel.SSet(tp,c)~=0 then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
 		e1:SetValue(LOCATION_REMOVED)
 		c:RegisterEffect(e1)
