6a58ab025c27028232e2f986e540e1a1e8e544ac
polymer1212 <937868195@qq.com>
2026-06-03 00:29:25 +0800

    fix

--- expansions/script/c29016278.lua
+++ expansions/script/c29016278.lua
@@ -2,10 +2,11 @@
 function c29016278.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetCategory(CATEGORY_HANDES+CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCountLimit(1,29016278+EFFECT_COUNT_CODE_OATH)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
 	e1:SetTarget(c29016278.target)
 	e1:SetOperation(c29016278.activate)
 	c:RegisterEffect(e1)
@@ -17,18 +18,23 @@ function c29016278.filter2(c)
 	return c:IsSetCard(0x67af) and c:IsType(TYPE_TRAP) and c:IsAbleToHand()
 end
 function c29016278.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c29016278.filter1,tp,LOCATION_DECK,0,1,nil) and Duel.IsExistingMatchingCard(c29016278.filter2,tp,LOCATION_DECK,0,1,nil) and Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)>0 end
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler(),REASON_EFFECT)
+		and Duel.IsExistingMatchingCard(c29016278.filter1,tp,LOCATION_DECK,0,1,nil)
+		and Duel.IsExistingMatchingCard(c29016278.filter2,tp,LOCATION_DECK,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,tp,1)
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_DECK)
 end
 function c29016278.activate(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.DiscardHand(tp,aux.TRUE,1,1,REASON_EFFECT+REASON_DISCARD)~=1 then return false end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c29016278.filter1,tp,LOCATION_DECK,0,1,1,nil)
-	local g1=Duel.SelectMatchingCard(tp,c29016278.filter2,tp,LOCATION_DECK,0,1,1,nil)
-	g1:Merge(g1,g)
-	if g1:GetCount()==2 then
-		Duel.SendtoHand(g1,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,g)
+	if Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_EFFECT+REASON_DISCARD,nil,REASON_EFFECT)==0 then return end
+	local g1=Duel.GetMatchingGroup(c29016278.filter1,tp,LOCATION_DECK,0,nil)
+	local g2=Duel.GetMatchingGroup(c29016278.filter2,tp,LOCATION_DECK,0,nil)
+	if g1:GetCount()>0 and g2:GetCount()>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg1=g1:Select(tp,1,1,nil)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg2=g2:Select(tp,1,1,nil)
+		sg1:Merge(sg2)
+		Duel.SendtoHand(sg1,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg1)
 	end
 end
