0823d40f97b92b1cdc0023b1b02de4631f1fdaac
polymer1212 <937868195@qq.com>
2026-06-21 01:11:27 +0800

    fix

--- expansions/script/c71400037.lua
+++ expansions/script/c71400037.lua
@@ -22,7 +22,7 @@ function c71400037.initial_effect(c)
 	e2:SetOperation(c71400037.op1)
 	c:RegisterEffect(e2)
 end
-function c71400037.filter(c,e,tp)
+function c71400037.filter1(c,e,tp)
 	return c:IsSetCard(0x714) and c:IsLevel(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
 end
 function c71400037.filter1a(c)
@@ -31,13 +31,13 @@ end
 function c71400037.tg1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c71400037.filter(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(c71400037.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+		and Duel.IsExistingTarget(c71400037.filter1,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
 	local tg=1
-	if Duel.IsExistingMatchingCard(c71400037.filter1,tp,LOCATION_MZONE+LOCATION_GRAVE,LOCATION_MZONE+LOCATION_GRAVE,1,nil) then
+	if Duel.IsExistingMatchingCard(c71400037.filter1a,tp,LOCATION_MZONE+LOCATION_GRAVE,LOCATION_MZONE+LOCATION_GRAVE,1,nil) then
 		tg=2
 	end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c71400037.filter,tp,LOCATION_GRAVE,0,1,tg,nil,e,tp)
+	local g=Duel.SelectTarget(tp,c71400037.filter1,tp,LOCATION_GRAVE,0,1,tg,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 function c71400037.op1(e,tp,eg,ep,ev,re,r,rp)
